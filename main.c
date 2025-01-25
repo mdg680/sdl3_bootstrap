43ca28d98f252315bfc03ab876e3a8fc0b9f26bb
mdg680 <monoper@gmai.com>
2025-01-25 21:55:13 +0100

    Update: use SDL3 callbacks for main example
    Using callback instead of an oldschool main function, leverages SDL3's
    framework better. Now the main file example has init, iterate and
    quit blocks with no main function necessary.

--- main.c
+++ main.c
@@ -1,32 +1,61 @@
+
+#define SDL_MAIN_USE_CALLBACKS 1 /* see: https://wiki.libsdl.org/SDL3/README/main-functions */
+
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-// ...existing code...
-int main(int argc, char *argv[]) {
-    // ...existing code...
-    SDL_Window *window = SDL_CreateWindow("SDL3 Window", 680, 480, SDL_WINDOW_OPENGL);
-    if (!window) {
-        printf("Error creating window: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+static Uint64 last_time = 0;
+
+/* Window dimensions */
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+/* Initialises application, runs once at startup */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    SDL_SetAppMetadata("Mandelbrot set explorer using SDL3", "0.1", "com.monoper.mandelbrot");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-    if (!renderer) {
-        printf("Error creating renderer: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
+    if (!SDL_CreateWindowAndRenderer("Mandelbrot SDL3", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
 
-    SDL_Delay(3000);
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    /* as you can see from this, rendering draws over whatever was drawn before it. */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black, full alpha */
+    SDL_RenderClear(renderer);  /* start with a blank canvas. */
+
+    SDL_RenderPresent(renderer);  /* put it all on the screen! */
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
